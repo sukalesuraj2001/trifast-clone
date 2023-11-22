f391fdcce2b01a913a066a2308b4a74688aefe86
Suraj <srsukale20@gmail.com>
2023-11-22 09:40:00 +0530

    chnging navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+ 
 })
 export class NavbarComponent {
 
@@ -15,12 +17,12 @@ export class NavbarComponent {
 
 isbacked=false
 
-  isToggled = false;
+  
+  isToggled: boolean = false;
 
   btn() {
     this.isToggled = !this.isToggled;
   }
-
 
 
 
